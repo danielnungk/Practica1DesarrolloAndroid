dad621ef4dd1a4cf526a2b2c9325119c0eafebc6
Dan <daniel1_nunez@hotmail.com>
2025-09-02 22:43:07 -0600

    Practica 1

--- app/src/main/java/dan/proyecto/practica1/MainActivity.kt
+++ app/src/main/java/dan/proyecto/practica1/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import dan.proyecto.practica1.ui.theme.Practica1Theme
 import androidx.compose.ui.unit.sp
