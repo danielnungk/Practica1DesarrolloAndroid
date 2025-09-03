b783f24fb16ec01402536baa9c9ac927db9b2e83
Dan <daniel1_nunez@hotmail.com>
2025-09-02 22:42:49 -0600

    Practica 1

--- app/src/main/java/dan/proyecto/practica1/MainActivity.kt
+++ app/src/main/java/dan/proyecto/practica1/MainActivity.kt
@@ -1,17 +1,28 @@
 package dan.proyecto.practica1
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import dan.proyecto.practica1.ui.theme.Practica1Theme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +30,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Practica1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android", //22131298
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Columna()
             }
         }
     }
@@ -43,5 +49,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Practica1Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun Columna(){
+    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+
+        Text(text="Jose Daniel Nuñez Gomez",
+            fontSize = 28.sp, fontFamily = FontFamily.Default,
+            color = Color(35,171,173), fontWeight = FontWeight.Bold
+        )
+        Text(text="22131298",
+            fontSize = 24.sp, fontFamily = FontFamily.SansSerif,
+            color = Color(173,37,35), fontWeight = FontWeight.Bold
+        )
     }
 }
